f90d3ff77d4890533047029dd78ff570dc29fb43
Bob Sun <2428059449@qq.com>
2025-08-09 14:43:53 +0800

    token拦截器的编写

---
+++ api-passenger/src/main/java/com/bob/apipassenger/interceptor/JwtInterceptor.java
@@ -0,0 +1,50 @@
+package com.bob.apipassenger.interceptor;
+
+import com.auth0.jwt.exceptions.AlgorithmMismatchException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.bob.internalcommon.constant.dto.ResponseResult;
+import com.bob.internalcommon.constant.util.JwtUtils;
+import net.sf.json.JSONObject;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+/**
+ * Created by Sun on 2025/8/9.
+ * Description:
+ */
+public class JwtInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
+
+        boolean result = true;
+        String resultString = "";
+        String token = request.getHeader("Authorization");
+
+        try{
+            JwtUtils.parseToken(token);
+        }catch (SignatureVerificationException e){
+            resultString = "token sign error";
+            result = false;
+        }catch (TokenExpiredException e){
+            resultString = "token time out";
+            result = false;
+        }catch (AlgorithmMismatchException e){
+            resultString = "token AlgorithmMismatchException";
+            result = false;
+        }catch (Exception e){
+            resultString = "token invalic";
+            result = false;
+        }
+
+        if(!result){
+            PrintWriter out = response.getWriter();
+            out.print(JSONObject.fromObject(ResponseResult.fail(resultString)).toString());
+        }
+
+        return result;
+    }
+}
