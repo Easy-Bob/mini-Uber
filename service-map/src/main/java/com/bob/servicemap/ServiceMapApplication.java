b4f818175b9109ee3ea1a2434ffd5b0c2e1a7ca6
Bob Sun <2428059449@qq.com>
2025-08-10 14:58:01 +0800

    完成service-map创建

---
+++ service-map/src/main/java/com/bob/servicemap/ServiceMapApplication.java
@@ -0,0 +1,15 @@
+package com.bob.servicemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Sun on 2025/8/10.
+ * Description:
+ */
+@SpringBootApplication
+public class ServiceMapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceMapApplication.class, args);
+    }
+}
