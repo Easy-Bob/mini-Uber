8c4c40a8d3ac5b5d725e84007ebfd935cd63d9c2
Bob Sun <2428059449@qq.com>
2025-08-12 13:25:10 +0800

    第五节： 逆向工程 生成car对象，并作 统一化修改

---
+++ service-driver-user/src/main/java/com/bob/serviceDriverUser/controller/CarController.java
@@ -0,0 +1,20 @@
+package com.bob.serviceDriverUser.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author Bob Sun
+ * @since 2025-08-12
+ */
+@Controller
+@RequestMapping("/car")
+public class CarController {
+
+}
---
+++ service-driver-user/src/main/java/com/bob/serviceDriverUser/mapper/CarMapper.java
@@ -0,0 +1,18 @@
+package com.bob.serviceDriverUser.mapper;
+
+import com.bob.internalcommon.constant.dto.Car;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Bob Sun
+ * @since 2025-08-12
+ */
+@Repository
+public interface CarMapper extends BaseMapper<Car> {
+
+}
